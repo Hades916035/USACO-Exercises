8cbe639ad760b6c79f7c95287c489d5160d5c653
Priyanshu Lal <115842925+Hades916035@users.noreply.github.com>
2024-07-14 13:45:14 -0500

    simplify

--- ArmstrongNum.java
+++ ArmstrongNum.java
@@ -4,11 +4,12 @@ public class ArmstrongNum {
     public static void main(String[] args) {
         Scanner kb = new Scanner(System.in);
         System.out.print("Number: ");
-        String num = kb.next();
+        int n = kb.nextInt();
+        String num = Integer.toString(n);
         int a = (int)(num.charAt(0)) - 48;
         int b = (int)(num.charAt(1)) - 48;
         int c = (int)(num.charAt(2)) - 48;
-        boolean armstrong = (Math.pow(a, 3) + Math.pow(b, 3) + Math.pow(c, 3) == (100*a + 10*b + c));
+        boolean armstrong = (Math.pow(a, 3) + Math.pow(b, 3) + Math.pow(c, 3) == (n));
         System.out.println(armstrong);
 
         kb.close();
