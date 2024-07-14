a73de53605b166e688ca2c9528d14a3e2e5cce4f
Priyanshu Lal <115842925+Hades916035@users.noreply.github.com>
2024-07-14 00:28:42 -0500

    update file

--- MultiplicationTable.java
+++ MultiplicationTable.java
@@ -1,10 +1,23 @@
-<<<<<<< HEAD
 import java.util.*;
 
-=======
->>>>>>> 725934f5f60b2ceb0a540ade77f955ea7d41ca3c
 public class MultiplicationTable {
     public static void main(String[] args) {
-        
+        Scanner kb = new Scanner(System.in);
+        int num = kb.nextInt();
+        int arr[] = table(num);
+    }
+
+    public static int[] table(int num) {
+        int arr[] = new int[10];
+        arr[0] = num;
+        System.out.print("\n" + num);
+        for(int i=1; i<10; i++) {
+            arr[i] = arr[i-1] + num;
+            System.out.print(" " + arr[i]);
+        }
+
+
+        System.out.println();
+        return arr;
     }
 }
