dab6080721a06667309537f2e9c9cf3f1e867699
Priyanshu Lal <115842925+Hades916035@users.noreply.github.com>
2024-07-26 19:57:24 -0500

    finish

--- AmountIncreasing.java
+++ AmountIncreasing.java
@@ -1,10 +1,14 @@
+import java.util.*;
+
 public class AmountIncreasing {
     public static void main(String[] args) {
-        int arr[] = {2, 3, 4, 5, 1, 6, 7, 0, 8, 9};
-        System.out.println(maxSize(arr));
+        int arr1[] = {2, 3, 4, 5, 1, 6, 7, 0, 8, 9};
+        int arr2[] = {2, 0, -1, -2};
+        System.out.println(maxSize(arr1));
+        System.out.println(maxSize(arr2));
     }
 
-    public static void maxSize(int[] arr) {
+    public static int maxSize(int[] arr) {
         int maxSize = 1;
         int size = 1;
         for(int i=1; i<arr.length; i++) {
@@ -15,5 +19,7 @@ public class AmountIncreasing {
             }
             maxSize = Math.max(maxSize, size);
         }
+
+        return maxSize;
     }
 }
