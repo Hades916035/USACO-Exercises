6d852833c5c8096d47c8de20ea1b4886a674ff82
Priyanshu Lal <115842925+Hades916035@users.noreply.github.com>
2024-11-09 15:23:09 -0600

    finish

--- AdviksBinarySearch.py
+++ AdviksBinarySearch.py
@@ -1,17 +1,34 @@
+# def binary(x):
+#     arr=[1,2,3,4,5,6,7,8,9,10]
+#     rpar=len(arr)-1
+#     lpar=0
+#     ind=0
+#     found=False
+#     while found==False:
+#         if arr[(rpar+lpar)//2]>x:
+#             lpar=(rpar+lpar)//2
+#         elif arr[(rpar+lpar)//2]<x:
+#             rpar=(rpar+lpar)//2
+#         elif arr[(rpar+lpar)//2]==x:
+#             ind=((rpar+lpar)//2)
+#             print(ind)
+#             return ind
+
+# binary(7)
+
 def binary(x):
     arr=[1,2,3,4,5,6,7,8,9,10]
-    rpar=len(arr)-1
     lpar=0
-    ind=0
+    rpar=len(arr)-1
     found=False
+
     while found==False:
         if arr[(rpar+lpar)//2]>x:
-            lpar=(rpar+lpar)//2
-        elif arr[(rpar+lpar)//2]<x:
             rpar=(rpar+lpar)//2
+        elif arr[(rpar+lpar)//2]<x:
+            lpar=(rpar+lpar)//2
         elif arr[(rpar+lpar)//2]==x:
-            ind=((rpar+lpar)//2)
-            print(ind)
-            return ind
+            print((rpar+lpar)//2)
+            break;
 
-binary(7)
+binary(8)
