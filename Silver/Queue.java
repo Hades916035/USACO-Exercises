bf3e1c3f24c4623c2f2e0c971d30cbfdf0168062
Priyanshu Lal <115842925+Hades916035@users.noreply.github.com>
2024-12-26 16:11:33 -0600

    finish queue

--- Silver/Queue.java
+++ Silver/Queue.java
@@ -1,6 +1,7 @@
 public class Queue {
     private int[] arr;
     private String[] words;
+    private int[] endPoints = {0,0};
     private int type=0;
 
     public Queue() {
@@ -9,14 +10,44 @@ public class Queue {
     }
 
     public void add(int i) {
+        type=-1;
+        arr[endPoints[1]] = i;
 
+        endPoints[1]++;
+    }
+    
+    public void add(String i) {
+        type=1;
+        words[endPoints[1]] = i;
     }
 
     public void remove() {
-
+        if(endPoints[0] < endPoints[1])
+            endPoints[0]++;
     }
 
     public int peek() {
+        return arr[endPoints[1]-1];
+    }
 
+    public String top() {
+        return words[endPoints[1]-1];
+
+    }
+
+    public void printQueue() {
+        if(type==-1) {
+            System.out.println("Queue: ");
+            for(int j=endPoints[0]; j<endPoints[1]; j++) {
+                System.out.print(arr[j] + " ");
+            }
+            System.out.println();
+        } else if(type==1) {
+            System.out.println("Queue: ");
+            for(int j=endPoints[0]; j<endPoints[1]; j++) {
+                System.out.print(words[j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
--- Silver/Test.java
+++ Silver/Test.java
@@ -1,15 +1,33 @@
 public class Test {
     public static void main(String[] args) {
-        Stack s = new Stack();
+        // Stack s = new Stack();
 
-        s.push("s");
-        s.push("a");
-        s.pop();
-        s.top();
-        s.pop();
-        s.pop();
-        s.push("d");
+        // s.push("s");
+        // s.push("a");
+        // s.pop();
+        // s.top();
+        // s.pop();
+        // s.pop();
+        // s.push("d");
 
-        System.out.println();
+        // System.out.println();
+
+        Queue q = new Queue();
+        q.add(1);
+        q.printQueue();
+        q.add(2);
+        q.printQueue();
+        System.out.println(q.peek());
+        q.printQueue();
+        q.remove();
+        q.printQueue();
+        System.out.println(q.peek());
+        q.printQueue();
+        q.remove();
+        q.printQueue();
+        q.remove();
+        q.printQueue();
+        q.add(1);
+        q.printQueue();
     }
 }
